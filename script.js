c80fe9232bb770ef29f7ac15ab39e3f24de45423
RolandsPolo <57348320+RolandsPolo@users.noreply.github.com>
2020-05-03 20:50:48 +0300

    check

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ tl.fromTo(hero, 1.3, { height: "0%" }, { height: "80%", ease: Power2.easeInOut }
     .fromTo(headline, 2, { opacity: "0" }, { opacity: "1", ease: Power2.easeInOut })
     .fromTo(heroImg, 2, { filter: "brightness(100%)" }, { filter: "brightness(20%)" })
     .fromTo(headline, 2, { top: "50%" }, { top: "70%", ease: Power2.easeInOut }, "-=1.2")
-    .fromTo(scroll, 2,{width:"0px"},{width:"80px"},"-=1.2")
-    .fromTo(scroll, 2,{height:"0px"},{height:"80px"},"-=1.2");
+    .fromTo(scroll, 2, { width: "0px" }, { width: "100px" }, "-=1.2")
+    .fromTo(scroll, 2, { height: "0px" }, { height: "100px" }, "-=1.2");
 
 
 
@@ -26,46 +26,45 @@ const controller = new ScrollMagic.Controller();
 const scene = new ScrollMagic.Scene({
     triggerElement: '.test',
     triggerHook: 0.5,
-    offset:-200,
+    offset: -200,
 })
-.setClassToggle(".test","show")
-.addTo(controller);
+    .setClassToggle(".test", "show")
+    .addTo(controller);
 
 
 
 const scene2 = new ScrollMagic.Scene({
     triggerElement: '.block-steve',
-    offset: -100,
 
 })
-.setClassToggle(".block-steve","change")
-.addTo(controller);
+    .setClassToggle(".block-steve", "change")
+    .addTo(controller);
 
 const scene3 = new ScrollMagic.Scene({
     triggerElement: '.secondBlock',
     offset: -200,
 })
-.setClassToggle(".secondBlock","afterChange")
-.addTo(controller);
+    .setClassToggle(".secondBlock", "afterChange")
+    .addTo(controller);
 
 const scene4 = new ScrollMagic.Scene({
     triggerElement: '.thirdBlock',
     offset: -200,
 })
-.setClassToggle(".thirdBlock","afterThisChange")
-.addTo(controller);
+    .setClassToggle(".thirdBlock", "afterThisChange")
+    .addTo(controller);
 
 
 const scene5 = new ScrollMagic.Scene({
-    triggerElement: '.fourthBlock', 
+    triggerElement: '.fourthBlock',
 })
-.setClassToggle(".fourthBlock","afterAnimate")
-.addTo(controller);
+    .setClassToggle(".fourthBlock", "afterAnimate")
+    .addTo(controller);
 
 const scene6 = new ScrollMagic.Scene({
-    triggerElement: '.cool-text', 
+    triggerElement: '.cool-text',
 })
-.setClassToggle(".cool-text","main")
-.addTo(controller);
+    .setClassToggle(".cool-text", "main")
+    .addTo(controller);
 
 
